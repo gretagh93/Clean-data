8455f3b8620ae14af4438adb70e7fc13d932f457
Greta Garcia Hernandez <gretatara@users.noreply.github.com>
2021-03-16 16:07:16 +0100

    Understand and Read data files.

--- scripts/run_analysis.R
+++ scripts/run_analysis.R
@@ -6,3 +6,22 @@
 # From the data set in step 4, creates a second, independent tidy data set 
 # with the average of each variable for each activity and each subject.
 
+# Read file tab separated values and "," used as decimals points.
+
+# subject_test.txt" y "subject_train.txt" - registros con las ID de cada sujeto.
+# y_test.txt" y "y_train.txt" - actividad de cada registro codificada como números.
+# activity_labels.txt - Significado de los números de las actividades "y".
+# X_test.txt y X_train.txt - marco de datos en el que cada columna es una medida.
+# features.txt - nombres de las medidas de "x".
+
+# mergedata <- merge(edu, dom, by.y="CountryCode", all=TRUE)
+
+X_test <- read.table("./data/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt")
+Y_test <- read.table("./data/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("./data/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/subject_test.txt")
+X_train <- read.table("./data/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/X_train.txt")
+Y_train <- read.table("./data/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/y_train.txt")
+subject_train <- read.table("./data/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/subject_train.txt")
+
+
+
